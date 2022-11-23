805baec17314b87980d9095bfd90237900b9ed17
Pawel Baran <pawelbaranc@gmail.com>
2022-11-23 21:00:48 +0100

    SettingsImplemented

---
+++ DaySchedulerApp/DaySchedulerApp.Application/Contracts/Infrastructure/IEmailSender.cs
@@ -0,0 +1,14 @@
+﻿using DaySchedulerApp.Application.Models.Email;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaySchedulerApp.Application.Contracts.Infrastructure
+{
+    public interface IEmailSender
+    {
+        Task<bool> SendEmail(Email Email);
+    }
+}
---
+++ DaySchedulerApp/DaySchedulerApp.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,23 @@
+﻿using DaySchedulerApp.Application.Models.Email;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaySchedulerApp.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+
+        public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<EmailSettings>(configuration.GetSection("EmailSender"));
+
+
+            return services;
+        }
+    }
+}
