608d702cf6c19de13f4a1f271943bd5a7db18500
Pawel Baran <pawelbaranc@gmail.com>
2022-12-01 19:53:46 +0100

    AssignmentService Test Start

---
+++ DaySchedulerApp/DaySchedulerApp.UnitTests/Assignments/AssignmentServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using DaySchedulerApp.Application.Contracts;
+using DaySchedulerApp.UnitTests.Mocks;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaySchedulerApp.UnitTests.Assignments
+{
+    public class AssignmentServiceTests
+    {
+        private readonly Mock<IAssignmentRepository> _mockRepo;
+
+        public AssignmentServiceTests()
+        {
+            _mockRepo = MockAssignmentRepository.GetAssignmentRepository();
+        }
+    }
+}
